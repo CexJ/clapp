ff4bdec362f9f40f1ef93ebc6b16a6c626aca8bb
ANDREACesaro <ANDREACesaro@DESKTOP-SM0O79S.lille.fr.ibm.com>
2019-11-28 16:42:12 +0100

    Add orFrom support

--- clapp/src/main/java/com/cexj/clapp/builder/IO_Read.java
+++ clapp/src/main/java/com/cexj/clapp/builder/IO_Read.java
@@ -25,6 +25,10 @@ public final class IO_Read<T,F extends FunctionFromFuture<T,?>,G extends Functio
 		return IO_Read.of(newIo);
 	}
 	
+	public IO_Read<T, F, G, R>orReadFrom(final IChannel<T> channel){
+		var newIo = io.orFrom(channel);
+		return IO_Read.of(newIo);
+	}
 	
 	public IO_Write<T, F, G, R> andWriteItTo(final OChannel<T> channel) {
 		var newIo = io.andWriteItTo(channel);
